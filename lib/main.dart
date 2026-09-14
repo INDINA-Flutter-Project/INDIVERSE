3ddc0c1f73a2353552eb5bbef64bed67147e70ca
FaisalAlanazi-1 <xhjoalenzi@gmail.com>
2026-09-14 18:03:20 +0300

    add supabase keys in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:indina/screens/splash/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/constants/app_theme.dart';
 
-void main() => runApp(const IndiverseApp());
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://wifydarxzssyqdodbcsd.supabase.co',
+    publishableKey: 'sb_publishable_FuhpeRcKmDa8e7YEoYyo9g__zRFxmJ0',
+  );
+
+  runApp(const IndiverseApp());
+}
 
 class IndiverseApp extends StatelessWidget {
   const IndiverseApp({super.key});
