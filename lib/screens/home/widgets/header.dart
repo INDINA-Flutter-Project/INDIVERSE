95958ba1c2a2e386a4c5941571fde3c8c615a073
Turki Mohammed <152296062+turki125@users.noreply.github.com>
2026-09-16 19:47:36 +0300

    fix: remove home header logo
    Keep the INDIVERSE title while removing the green gamepad logo.

--- lib/screens/home/widgets/header.dart
+++ lib/screens/home/widgets/header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:indina/core/constants/app_colors.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key});
@@ -8,18 +7,6 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 42,
-          height: 42,
-          decoration: BoxDecoration(
-            color: AppColors.primary,
-            borderRadius: BorderRadius.circular(13),
-          ),
-          child: const Icon(Icons.gamepad_rounded, color: Color(0xFF092117)),
-        ),
-
-        const SizedBox(width: 11),
-
         const Expanded(
           child: Text(
             'INDIVERSE',
